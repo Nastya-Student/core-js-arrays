629d5d72dbd37017629ce1d1a51028d10a16b364
Nastya Belova <nastjabelova0@gmail.com>
2025-11-14 01:22:19 +0300

    feat: implement getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -437,8 +437,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr2 = [];
+  numbers.filter((item, i) => {
+    if (item % 2 !== 0) {
+      arr2.push(i);
+    }
+    return item;
+  });
+  return arr2;
 }
 
 /**
